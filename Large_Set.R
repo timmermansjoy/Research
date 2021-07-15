946f1d327b48eda5c698962aa990923aadbc0773
gwenvdwall <gwenvandewall@hotmail.com>
2021-07-15 17:25:08 +0200

    Barplot all groups

--- Large_Set.R
+++ Large_Set.R
@@ -119,7 +119,14 @@ colnames(Grouped_Codes) <- .x
 Code_Counts$groups=group_names
 
 #combine Sufficient_base with Grouped_codes
-#All_Groups <- rbind(Grouped_Codes, Sufficient_Base)
+colnames(Sufficient_Base)<-colnames(Grouped_Codes)
+All_Groups <- rbind(Grouped_Codes, Sufficient_Base)
 
 #plot code occurrence
-ggplot(data=Sufficient_Base, aes(x=code, y=n)) + geom_bar(stat='identity', colour='black', fill='white')
+ggplot(data=All_Groups, aes(x=group, y=total)) + 
+  geom_bar(stat='identity', width = 0.8) + 
+  theme_minimal() + 
+  xlab ("ICD-10 Codes") + 
+  ylab ("Frequency") + 
+  geom_text(aes(label=total), vjust=-0.3, size=3.5) + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
