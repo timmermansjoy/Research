cb4f86b8067bb9a1568e7b5ecb8e08688f38a4d0
gwenvdwall <gwenvandewall@hotmail.com>
2021-08-24 14:12:47 +0200

    More Subsets

--- Subsets.R
+++ Subsets.R
@@ -21,8 +21,12 @@ Breast_Cancer <- subset(Data, Data$`ICD-10 Code` == 'C50.9')
   .Suc_Breast_Cancer <- dplyr::count(Breast_Cancer, Trial_Success)
   .Succes_Breast_Cancer <- Percentage_calc(.Suc_Breast_Cancer)
   
+  #Percentage Terminations
+  .term_Breast_Cancer <- subset_termination(.Suc_Breast_Cancer)
+  
   #add results into dataframe
   Subgroup_Success <- data.frame("Breast Cancer" = .Succes_Breast_Cancer)
+  Subgroup_Termination <- data.frame("Breast Cancer" = .term_Breast_Cancer)
   
   #Remove Breast Cancer from Data
   Data_Subset <- subset(Data, Data$`ICD-10 Code` != 'C50.9')
@@ -32,19 +36,45 @@ Breast_Cancer <- subset(Data, Data$`ICD-10 Code` == 'C50.9')
 #C
 C <- subset(Data_Subset, grepl("^C", Data_Subset$`ICD-10 Code`))
 .suc_C <- dplyr::count(C, Trial_Success)
-Subgroup_Success$Cancer <- Percentage_calc(.suc_C)  
+Subgroup_Success$Cancer <- Percentage_calc(.suc_C) 
+Subgroup_Termination$Cancer <- subset_termination(.suc_C)
 #remove C from dataset
+
+#B18.2
+Chronic_viral_hepatitis_C <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'B18.2')
+.suc_HepC <- dplyr::count(Chronic_viral_hepatitis_C, Trial_Success)
+Subgroup_Success$"Chronic Viral Hepatitis C" <- Percentage_calc(.suc_HepC)
+Subgroup_Termination$"Chronic Viral Hepatitis C" <- subset_termination(.suc_HepC)
+Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'B18.2')
+
+#B24
+HIV <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'B24')
+.suc_HIV <- dplyr::count(Chronic_viral_hepatitis_C, Trial_Success)
+Subgroup_Success$HIV<- Percentage_calc(.suc_HIV)
+Subgroup_Termination$HIV <- subset_termination(.suc_HIV)
+Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'B24')
+
+#A and B
+.A <- subset(Data_Subset, grepl("^A", Data_Subset$`ICD-10 Code`))
+.B <- subset(Data_Subset, grepl("^B", Data_Subset$`ICD-10 Code`))
+AB <- rbind(.A, .B)
+.suc_AB <- dplyr::count(AB, Trial_Success)
+Subgroup_Success$"Infectious and Parasitic Disease" <- Percentage_calc(.suc_AB) 
+Subgroup_Termination$"Infectious and Parasitic Disease" <- subset_termination(.suc_AB)
+#remove A and B from dataset
 
 #G30.9
 Alzheimer <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'G30.9')
 .suc_Alzheimer <- dplyr::count(Alzheimer, Trial_Success)
 Subgroup_Success$Alzheimer <- Percentage_calc(.suc_Alzheimer)
+Subgroup_Termination$Alzheimer <- subset_termination(.suc_Alzheimer)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'G30.9')
   
 #G
 G <- subset(Data_Subset, grepl("^G", Data_Subset$`ICD-10 Code`))
 .suc_G <- dplyr::count(G, Trial_Success)
 Subgroup_Success$"Nervous System" <- Percentage_calc_large(.suc_G)
+Subgroup_Termination$"Nervous System" <- subset_termination_large(.suc_G)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != grepl("^G", Data_Subset$`ICD-10 Code`))
 #removing "G" from the dataset does not work as of yet
 
