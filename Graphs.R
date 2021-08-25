2e24a4705881d96f7f0f53a52e852b1317f32f5e
gwenvdwall <gwenvandewall@hotmail.com>
2021-08-25 20:40:19 +0200

    More Graphs

--- Graphs.R
+++ Graphs.R
@@ -1,11 +1,10 @@
 #This script creates graphs from the data for visual analysis
 #Run the matching script before creating the graph
 
-#Packages
+#plot code occurrence, 
+#Associated Script: Grouping.R
 library(ggplot2)
-
-#plot code occurrence, Run Script: Grouping.R
-ggplot(data=All_Groups, aes(x=group, y=total, fill=substr(group, 0, 1))) + 
+ggplot(data = All_Groups, aes(x=group, y=total, fill=substr(group, 0, 1))) + 
   geom_bar(stat='identity', width = 0.8) + 
   theme_minimal() + 
   xlab ("ICD-10 Codes") + 
@@ -15,3 +14,23 @@ ggplot(data=All_Groups, aes(x=group, y=total, fill=substr(group, 0, 1))) +
 theme(legend.position = "none")
 
 #TO-DO: better colour pallete for the above graph
+
+#plot subgroup success
+#Associated script: Subsets.R
+library(ggplot2)
+ggplot(data = Subgroup_Success, aes(x = Field, y = Success)) +
+  geom_bar(stat = "identity", width = 0.8) +
+  theme_minimal() +
+  xlab ("Field of Research") +
+  ylab ("Percentage Positive Outcomes")
+  #Angle x-axis names: theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+#Plot subgroup termination
+#Associated script: Subsets.R
+library(ggplot2)
+ggplot(data = Subgroup_Termination, aes(x = Field, y = Termination)) +
+  geom_bar(stat = "identity", width = 0.8) +
+  theme_minimal() +
+  xlab ("Field of Research") +
+  ylab ("Percentage Terminated Trials")
+#Angle x-axis names: theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
--- Subsets.R
+++ Subsets.R
@@ -9,6 +9,7 @@ rm(list = ls(all.names = TRUE))
 library(readxl)
 library(dplyr)
 library(sjmisc)
+library(reshape2)
 source("Utils.R")
 
 #Import Data Set
@@ -17,16 +18,12 @@ Data<-read_xlsx('Data/Prelimenary_Data.xlsx')
 #Extract C50.9: Breast Cancer
 Breast_Cancer <- subset(Data, Data$`ICD-10 Code` == 'C50.9')
 
-  #Percentage positive outcomes
+  #Count success
   .Suc_Breast_Cancer <- dplyr::count(Breast_Cancer, Trial_Success)
-  .Succes_Breast_Cancer <- Percentage_calc(.Suc_Breast_Cancer)
-  
-  #Percentage Terminations
-  .term_Breast_Cancer <- subset_termination(.Suc_Breast_Cancer)
-  
-  #add results into dataframe
-  Subgroup_Success <- data.frame("Breast Cancer" = .Succes_Breast_Cancer)
-  Subgroup_Termination <- data.frame("Breast Cancer" = .term_Breast_Cancer)
+
+  #Calculate percentage success and termination and add to dataframe
+  Subgroup_Success <- data.frame(Field = "Breast Cancer", Success = Percentage_calc(.Suc_Breast_Cancer))
+  Subgroup_Termination <- data.frame(Field = "Breast Cancer", Termination = subset_termination(.Suc_Breast_Cancer))
   
   #Remove Breast Cancer from Data
   Data_Subset <- subset(Data, Data$`ICD-10 Code` != 'C50.9')
@@ -36,22 +33,28 @@ Breast_Cancer <- subset(Data, Data$`ICD-10 Code` == 'C50.9')
 #C
 C <- subset(Data_Subset, grepl("^C", Data_Subset$`ICD-10 Code`))
 .suc_C <- dplyr::count(C, Trial_Success)
-Subgroup_Success$Cancer <- Percentage_calc(.suc_C) 
-Subgroup_Termination$Cancer <- subset_termination(.suc_C)
+.z <- data.frame(Field = "Cancer", Success = Percentage_calc(.suc_C))
+.y <- data.frame(Field = "Cancer", Termination = subset_termination(.suc_C))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 #remove C from dataset
 
 #B18.2
 Chronic_viral_hepatitis_C <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'B18.2')
 .suc_HepC <- dplyr::count(Chronic_viral_hepatitis_C, Trial_Success)
-Subgroup_Success$"Chronic Viral Hepatitis C" <- Percentage_calc(.suc_HepC)
-Subgroup_Termination$"Chronic Viral Hepatitis C" <- subset_termination(.suc_HepC)
+.z <- data.frame(Field = "Chronic Viral Hepatitis C", Success = Percentage_calc(.suc_HepC))
+.y <- data.frame(Field = "Chronic Viral Hepatitis C", Termination = subset_termination(.suc_HepC))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'B18.2')
 
 #B24
 HIV <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'B24')
 .suc_HIV <- dplyr::count(Chronic_viral_hepatitis_C, Trial_Success)
-Subgroup_Success$HIV<- Percentage_calc(.suc_HIV)
-Subgroup_Termination$HIV <- subset_termination(.suc_HIV)
+.z <- data.frame(Field = "HIV", Success = Percentage_calc(.suc_HIV))
+.y <- data.frame(Field = "HIV", Termination = subset_termination(.suc_HIV))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'B24')
 
 #A and B
@@ -59,22 +62,29 @@ Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'B24')
 .B <- subset(Data_Subset, grepl("^B", Data_Subset$`ICD-10 Code`))
 AB <- rbind(.A, .B)
 .suc_AB <- dplyr::count(AB, Trial_Success)
-Subgroup_Success$"Infectious and Parasitic Disease" <- Percentage_calc(.suc_AB) 
-Subgroup_Termination$"Infectious and Parasitic Disease" <- subset_termination(.suc_AB)
+.z <- data.frame(Field = "Infectious and Parasitic Disease", Success = Percentage_calc(.suc_AB))
+.y <- data.frame(Field = "Infectious and Parasitic Disease", Termination = subset_termination(.suc_AB))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 #remove A and B from dataset
 
 #G30.9
 Alzheimer <- subset(Data_Subset, Data_Subset$`ICD-10 Code` == 'G30.9')
 .suc_Alzheimer <- dplyr::count(Alzheimer, Trial_Success)
-Subgroup_Success$Alzheimer <- Percentage_calc(.suc_Alzheimer)
-Subgroup_Termination$Alzheimer <- subset_termination(.suc_Alzheimer)
+.z <- data.frame(Field = "Alzheimer", Success = Percentage_calc(.suc_Alzheimer))
+.y <- data.frame(Field = "Alzheimer", Termination = subset_termination(.suc_Alzheimer))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != 'G30.9')
   
 #G
 G <- subset(Data_Subset, grepl("^G", Data_Subset$`ICD-10 Code`))
 .suc_G <- dplyr::count(G, Trial_Success)
-Subgroup_Success$"Nervous System" <- Percentage_calc_large(.suc_G)
-Subgroup_Termination$"Nervous System" <- subset_termination_large(.suc_G)
+.z <- data.frame(Field = "Nervous System", Success = Percentage_calc_large(.suc_G))
+.y <- data.frame(Field = "Nervous System", Termination = subset_termination_large(.suc_G))
+Subgroup_Success <- rbind(Subgroup_Success, .z)
+Subgroup_Termination <- rbind(Subgroup_Termination, .y)
 Data_Subset <- subset(Data_Subset, Data_Subset$`ICD-10 Code` != grepl("^G", Data_Subset$`ICD-10 Code`))
 #removing "G" from the dataset does not work as of yet
+
 
